e67f545007051bb34c69afc45fab1042de93df66
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-03-25 15:25:53 -0600

    Updated runtime port and host

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -7,8 +7,10 @@ python manage.py sendtestemail --admins
 python manage.py migrate --no-input 
 python manage.py auto_admin
 
-export RUNTIME_PORT=8080
+
+RUNTIME_PORT=${PORT:-8080}
+RUNTIME_HOST=${HOST:-0.0.0.0}
 
 # add our static files to container itself on runtime
 # python manage.py collectstatic --no-input
-gunicorn cfehome.wsgi:application --bind 0.0.0.0:$RUNTIME_PORT
+gunicorn cfehome.wsgi:application --bind $RUNTIME_HOST:$RUNTIME_PORT
